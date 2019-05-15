ae7c239c9d304420738f485bc3fb99d20255c698
Lorenzo Scarlino <lorenzo.scarlino.98@gmail.com>
2019-05-15 14:53:16 +0200

    Bozza semClose()

--- sorgente/disastrOS_semopen.c
+++ sorgente/disastrOS_semopen.c
@@ -10,6 +10,7 @@ void internal_semOpen(){
   int semnum = running -> syscall_args[0]; //IL PRIMO ARGOMENTO DELLA SYSCALL E' L'ID DEL SEMAFORO
   int counter = running -> syscall_args[1]; //IL SECONDO ARGOMENTO DELLA SYSCALL E' IL COUNTER DEL SEMAFORO (SEMNUM)
   
+  printf("Creo il semaforo %d...", semnum);
   
   Semaphore* aux = SemaphoreList_byId(&semaphores_list,semnum); //VERIFICO SE IL SEMAFORO (SEMNUM) E' GIA' PRESENTE NELLA LISTA DEI SEMAFORI
   
@@ -17,15 +18,14 @@ void internal_semOpen(){
 	  Semaphore* sem = Semaphore_alloc(semnum,counter);  //SE IL SEMAFORO (SEMNUM) NON ESISTE LO ALLOCO
 	  List_insert(&semaphores_list,semaphores_list.last,sem); //INSERISCO IL SEMAFORO (SEMNUM) ALLA LISTA DEI SEMAFORI
 	  }
-	  
-	  SemDescriptor* sfd = SemDescriptor_alloc(running -> last_sem_fd, sem, running); //IN OGNI CASO ALLOCO IL DESCRIPTOR DEL SEMAFORO (SEMNUM)
+  SemDescriptor* sfd = SemDescriptor_alloc(running -> last_sem_fd, sem, running); //IN OGNI CASO ALLOCO IL DESCRIPTOR DEL SEMAFORO (SEMNUM)
 	  
 	 
 	  
-	  SemDescriptorPtr* sfdptr = SemDescriptorPtr_alloc(sfd); //ALLOCO IL PUNTATORE AL DESCRITTORE (SFD) DEL SEMAFORO (SEMNUM) 
-	  
-	  List_insert(running->sem_descriptors, sem_descriptors.last, sfdptr); //AGGIUNGO IL PUNTATORE SFDPTR ALLA LISTA DEI PUNTATORI AI DESCRITTORI DEI SEMAFORI
-	  
-		
+  SemDescriptorPtr* sfdptr = SemDescriptorPtr_alloc(sfd); //ALLOCO IL PUNTATORE AL DESCRITTORE (SFD) DEL SEMAFORO (SEMNUM) 
+  
+  List_insert(running->sem_descriptors, sem_descriptors.last, sfdptr); //AGGIUNGO IL PUNTATORE SFDPTR ALLA LISTA DEI PUNTATORI AI DESCRITTORI DEI SEMAFORI
+	
+  printf("Fatto!\n");	  
 }
 
